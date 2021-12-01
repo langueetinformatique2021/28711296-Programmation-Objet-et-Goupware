e88b0bdea8879bdba20f90015b1845b79d1a4907
Moses <Moses@192.168.1.52>
2021-12-01 22:05:29 +0100

    devoir tp6

--- 28711296-Programmation-Objet-et-Goupware/src/tp6/testChat.java
+++ 28711296-Programmation-Objet-et-Goupware/src/tp6/testChat.java
@@ -1,11 +1,18 @@
 package tp6;
 
+import atelier06.Chat;
+
 public class testChat {
 	
+	Chat c = new Chat();
 	Chat c = new Chat();
 	c.présente();
 	c.crie();
 	
+	c.domestiquer("Filou");
+	c.présente();
+	c.crie();
+
 	
 
 }
